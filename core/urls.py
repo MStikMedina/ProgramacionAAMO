9f34deefcb31adb7a358362ccdcb497f24f71f19
Marlon Medina <mm345099@gmail.com>
2026-02-24 07:54:34 -0500

    feat: implementacion de sidebar lateral y nueva pagina de inicio

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from gestion_datos.views import gestion_libros, gestion_colegios, gestion_profesores
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('libros/', gestion_libros, name='gestion_libros'),
     path('colegios/', gestion_colegios, name='gestion_colegios'),
