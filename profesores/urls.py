8c3f7c0d2f7e8bc973d28739146cb0775cf77547
Marlon Medina <mm345099@gmail.com>
2026-02-24 15:40:59 -0500

    feat: conectar link de Profesores en el menú lateral

--- profesores/urls.py
+++ profesores/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('horario/', views.ver_horario, name='ver_horario'),
+    path('', views.ver_horario, name='ver_horario'),
 ]
